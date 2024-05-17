ad71097c6ba804e52da171859bb72f4b33381b22
Kisor-S <kishorevk910@gmail.com>
2024-05-17 20:34:47 +0530

    Create function for addition, subtraction, multiplication and division

---
+++ script.js
@@ -0,0 +1,20 @@
+let firstNumber = 0;
+let secondNumber = 0;
+let operate = "";
+
+
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+}
+
+function multiply(a, b){
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
